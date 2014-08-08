5467dc0c10b09781f9b72bdb3c0c3fe644b8f502
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-08 23:48:20 +0100

    Adding dependency to lands-java-lib

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [
+                  [org.clojure/clojure "1.6.0"]
+                  [com.github.lands/lands-java-lib "0.1-SNAPSHOT"]]
   :main ^:skip-aot civs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/civs/core_test.clj
+++ test/civs/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [civs.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest langGenerationWorks
+  (let [l (com.github.langgen.SamplesBasedLanguageFactory/getRandomLanguage)]
+    (is (not (empty? (.name l))))))
