0d9e36409cee8a5c7aa14691a829152ef54f3747
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-08 23:55:41 +0100

    Testing loading worlds

--- test/civs/core_test.clj
+++ test/civs/core_test.clj
@@ -5,3 +5,9 @@
 (deftest langGenerationWorks
   (let [l (com.github.langgen.SamplesBasedLanguageFactory/getRandomLanguage)]
     (is (not (empty? (.name l))))))
+
+(deftest loadWorld
+  (let [filename "examples-worlds/seed_77.world"
+        f (java.io.File. filename)
+        w (. com.github.lands.PickleSerialization loadWorld f)]
+    (is (= "seed_77" (.getName w)))))
