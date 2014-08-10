4ad437782bf2fef4167200248992eb03515ca045
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-10 12:09:59 +0100

    Improving model tests

---
+++ test/civs/logic/demographics_test.clj
@@ -0,0 +1,15 @@
+(ns civs.logic.demographics-test
+  (:require [clojure.test :refer :all]
+            [civs.core :refer :all]
+            [civs.model :refer :all]
+            [civs.logic :refer :all]
+            [civs.logic.basic :refer :all]
+            [civs.logic.demographics :refer :all]
+            [civs.logic.tribe-choices :refer :all]))
+
+(deftest testCreateTribe
+  (let [g (create-game nil)
+        g (:game (create-tribe g "Tribe1" nil nil nil))
+        t (get (-> g .tribes) 1)]
+  (is t)
+  (is (= 1 (.id t)))))
--- test/civs/model_test.clj
+++ test/civs/model_test.clj
@@ -5,9 +5,24 @@
             [civs.logic :refer :all]
             [civs.logic.basic :refer :all]
             [civs.logic.demographics :refer :all]
-            [civs.logic.tribe-choices :refer :all]))
+            [civs.logic.tribe-choices :refer :all])
+  (:import [civs.model Population Tribe]))
 
-(deftest testCreateTribe
+(deftest test-total-persons
+  (is (= 15 (total-persons (Population. 1 2 3 4 5)))))
+
+(deftest test-active-persons
+  (is (= 5 (active-persons (Population. 1 2 3 4 5)))))
+
+(deftest test-is-dead?
+  (is (= false (is-dead? (Tribe. nil nil nil (Population. 1 2 3 4 5) nil))))
+  (is (= true  (is-dead? (Tribe. nil nil nil (Population. 0 0 0 0 0) nil)))))
+
+(deftest test-alive?
+  (is (= true  (alive?  (Tribe. nil nil nil (Population. 1 2 3 4 5) nil))))
+  (is (= false (alive?  (Tribe. nil nil nil (Population. 0 0 0 0 0) nil)))))
+
+(deftest test-create-tribe
   (let [g (create-game nil)
         g (:game (create-tribe g "Tribe1" nil nil nil))
         t (get (-> g .tribes) 1)]
