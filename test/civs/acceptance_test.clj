8102a7b47f823c54d5c6ec48c1e97f73355373ae
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-12 22:25:29 +0100

    Refactoring acceptance tests

--- test/civs/acceptance_test.clj
+++ test/civs/acceptance_test.clj
@@ -14,81 +14,65 @@
 
 (def verbose-acceptance-tests true)
 
-(deftest test-not-everyone-dies
+(defn mock-crand-int []
+  (let [my-r (Random. 1)]
+    (fn [n] (.nextInt my-r n))))
+
+(defn mock-crand-float []
+  (let [my-r (Random. 1)]
+    (fn [] (.nextFloat my-r))))
+
+(defn mock-land-biome [desired-biome]
+  (fn [world pos] (if (isLand world pos) desired-biome com.github.lands.Biome/OCEAN)))
+
+(deftest test-not-everyone-dies-immediately
+  (with-redefs )
   (let [g0 (generate-game w77 10)
-        g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))]
+        g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))
+        crand-int   mock-crand-int
+        crand-float mock-crand-float]
     (is (> (game-total-pop g) 0))))
 
 (deftest test-population-do-not-expand-too-much-too-fast
   (let [g0 (generate-game w77 10)
         start-pop (game-total-pop g0)
-        g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))]
+        g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))
+        crand-int  mock-crand-int
+        rand-float mock-crand-float]
     (is (< (game-total-pop g) (* start-pop 2)))))
 
-(defn mock-land-biome [desired-biome]
-  (fn [world pos] (if (isLand world pos) desired-biome com.github.lands.Biome/OCEAN)))
-
 ; We do not want to drop too drammatically and it should not increase too much
-(deftest test-population-in-a-sand-desert
-  (with-redefs [biome-at (mock-land-biome com.github.lands.Biome/SAND_DESERT)]
-    (let [g0 (generate-game w77 30)
+
+(defn check-biome biome min-factor max-factor ntribes nturns
+  (with-redefs [
+                 biome-at (mock-land-biome biome)
+                 crand-int   mock-crand-int
+                 crand-float mock-crand-float]
+    (let [g0 (generate-game w77 ntribes)
           start-pop (game-total-pop g0)
-          g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))
+          g (reduce (fn [g _] (turn g)) g0 (repeat nturns :_))
           final-pop (game-total-pop g)]
       (when verbose-acceptance-tests
-        (println "sand-desert scenario: initial population " start-pop)
-        (println "sand-desert scenario: final population "   final-pop))
-      (is (> final-pop (* start-pop 0.5)))
-      (is (< final-pop (* start-pop 1.3))))))
+        (println (.toString biome) " scenario: initial population " start-pop)
+        (println (.toString biome) " scenario: final population "   final-pop))
+      (is (> final-pop (* start-pop min-factor)))
+      (is (< final-pop (* start-pop max-factor))))))
+
+(deftest test-population-in-a-sand-desert
+  (check-biome com.github.lands.Biome/SAND_DESERT 0.5 1.3 30 10))
 
 ; We do not want to drop too drammatically and it should not increase too much
 (deftest test-population-in-a-grassland
-  (with-redefs  [biome-at (mock-land-biome com.github.lands.Biome/GRASSLAND)]
-    (let [g0 (generate-game w77 30)
-          start-pop (game-total-pop g0)
-          g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))
-          final-pop (game-total-pop g)]
-      (when verbose-acceptance-tests
-        (println "grassland scenario: initial population " start-pop)
-        (println "grassland scenario: final population "   final-pop))
-      (is (> final-pop (* start-pop 0.9)))
-      (is (< final-pop (* start-pop 1.7))))))
+  (check-biome com.github.lands.Biome/GRASSLAND 0.9 1.7 30 10))
 
 ; We do not want to drop too drammatically and it should not increase too much
 (deftest test-population-in-a-savanna
-  (with-redefs  [biome-at (mock-land-biome com.github.lands.Biome/SAVANNA)]
-    (let [g0 (generate-game w77 30)
-          start-pop (game-total-pop g0)
-          g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))
-          final-pop (game-total-pop g)]
-      (when verbose-acceptance-tests
-        (println "savanna scenario: initial population " start-pop)
-        (println "savanna scenario: final population "   final-pop))
-      (is (> final-pop (* start-pop 0.9)))
-      (is (< final-pop (* start-pop 1.5))))))
+  (check-biome com.github.lands.Biome/SAVANNA 0.9 1.5 30 10))
 
 ; We do not want to drop too drammatically and it should not increase too much
 (deftest test-population-in-a-forest
-  (with-redefs  [biome-at (mock-land-biome com.github.lands.Biome/FOREST)]
-    (let [g0 (generate-game w77 30)
-          start-pop (game-total-pop g0)
-          g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))
-          final-pop (game-total-pop g)]
-      (when verbose-acceptance-tests
-        (println "forest scenario: initial population " start-pop)
-        (println "forest scenario: final population "   final-pop))
-      (is (> final-pop (* start-pop 0.9)))
-      (is (< final-pop (* start-pop 1.6))))))
+  (check-biome com.github.lands.Biome/FOREST 0.9 1.6 30 10))
 
 ; We do not want to drop too drammatically and it should not increase too much
 (deftest test-population-in-a-jungle
-  (with-redefs  [biome-at (mock-land-biome com.github.lands.Biome/JUNGLE)]
-    (let [g0 (generate-game w77 30)
-          start-pop (game-total-pop g0)
-          g (reduce (fn [g _] (turn g)) g0 (repeat 10 :_))
-          final-pop (game-total-pop g)]
-      (when verbose-acceptance-tests
-        (println "jungle scenario: initial population " start-pop)
-        (println "jungle scenario: final population "   final-pop))
-      (is (> final-pop (* start-pop 0.9)))
-      (is (< final-pop (* start-pop 1.7))))))
+  (check-biome com.github.lands.Biome/JUNGLE 0.9 1.7 30 10))
