f4c63b12c306045f10e23bbdb8ca597b55cf43a2
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-12 23:01:04 +0100

    Incomplete cli support sketched

--- src/civs/core.clj
+++ src/civs/core.clj
@@ -10,12 +10,43 @@
     [civs.logic.stats :refer :all]
     [civs.society :refer :all]
     [civs.graphics :refer :all]
-    [clojure.tools.cli :refer [parse-opts]]))
+    [clojure.tools.cli :refer [parse-opts]]
+    [clojure.string :as string]))
 
 ;(require '[civs.model :refer :all])
 ;(require '[civs.logic :refer :all])
 
 (def w (load-world "examples-worlds/seed_77.world"))
 
+(defn failure [msg]
+  (binding [*out* *err*]
+    (println "Error:" msg)
+    (println "")
+    (println "Use -h for help")
+    (println "Exit."))
+  (System/exit 1))
+
+(defn usage [options-summary]
+  (println (->> ["This program run simulations of civilizations evolution and struggling"
+        ""
+        "Usage: [lein run] civs [options]"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Feel free to ask all possible questions on https://github.com/ftomassetti/civs (just open an issue!)"]
+    (string/join \newline)))
+    (System/exit 0))
+
 (defn -main [& args]
-  (parse-opts args cli-options))
+  (println " Civs : a civilizations simulator ")
+  (println "----------------------------------")
+  (println "")
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (nil? (:world options))
+        (failure "World to be used not specified (option -w missing)")
+      (:help options)
+        (usage summary)
+      errors (failure errors))
+      (println "Not yet implemented...")))
