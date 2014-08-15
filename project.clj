06fcb6b878448e5c695d8ddba219f142fe7067e8
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-15 19:19:23 +0100

    Removing wrong license

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject civs "0.1.0-SNAPSHOT"
   :description "A simulator of civilizations evolution"
   :url "https://github.com/ftomassetti/civs"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                   [org.clojure/clojure "1.6.0"]
                   [com.github.lands/lands-java-lib "0.3-SNAPSHOT"]
