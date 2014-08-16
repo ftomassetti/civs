e386e34053f2960c0e14cca0975d7512e209f6d6
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-16 09:09:54 +0100

    Moving to 0.2 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject civs "0.1.0"
+(defproject civs "0.2.0-SNAPSHOT"
   :description "A simulator of civilizations evolution"
   :url "https://github.com/ftomassetti/civs"
   :license {:name "Apache License v 2.0"
