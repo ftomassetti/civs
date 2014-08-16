e237d45ba09fd6922dde7722f86b12e80bd04223
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-16 15:22:27 +0100

    Removing dead tribes

--- src/civs/logic.clj
+++ src/civs/logic.clj
@@ -18,8 +18,15 @@
          game (reduce (fn [acc, _] (:game (generate-tribe acc))) game (repeat n-tribes :just_something))]
     game))
 
+(defn- clean-game
+  "Remove dead tribes"
+  [game]
+  (let [tribes-map         (.tribes game)
+        updated-tribes-map (select-keys tribes-map (for [[id tribe] tribes-map :when (alive? tribe)] id))]
+    (assoc game :tribes updated-tribes-map)))
+
 (defn turn [game]
   (let [ tribes (vals (.tribes game))]
-    (reduce (fn [acc t] (tribe-turn acc t)) game tribes)))
+    (clean-game (reduce (fn [acc t] (tribe-turn acc t)) game tribes))))
 
 ;(repeatedly 100 (fn [] (def g (turn g))))
