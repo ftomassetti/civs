aaf280b20d9710b394825a1f4d4617ecbb10db5e
Federico Tomassetti <f.tomassetti@gmail.com>
2014-09-07 19:33:58 +0100

    Fix civs.model.society-test

--- test/civs/model/society_test.clj
+++ test/civs/model/society_test.clj
@@ -13,18 +13,18 @@
 
 (deftest a-small-band-should-have-a-low-probability-of-splitting
   (let [ g (create-game nil)
-         t (:tribe (create-tribe g "unnamed" {:x 15 :y 18} (Population. 1 1 1 1 1) initial-culture :band))
+         {t :tribe g :game} (create-tribe g "unnamed" {:x 15 :y 18} (Population. 1 1 1 1 1) initial-culture :band)
          p (possibility-of-splitting g t)]
     (is (< p 0.05))))
 
 (deftest a-very-large-band-should-have-a-high-probability-of-splitting
   (let [ g (create-game nil)
-         t (:tribe (create-tribe g "unnamed" {:x 15 :y 18} (Population. 40 40 40 40 40) initial-culture :band))
+         {t :tribe g :game} (create-tribe g "unnamed" {:x 15 :y 18} (Population. 40 40 40 40 40) initial-culture :band)
          p (possibility-of-splitting g t)]
     (is (> p 0.50))))
 
 (deftest a-small-tribe-should-have-a-low-probability-of-splitting
   (let [ g (create-game nil)
-         t (:tribe (create-tribe g "unnamed" {:x 15 :y 18} (Population. 15 15 15 15 15) initial-culture :tribe))
+         {t :tribe g :game} (create-tribe g "unnamed" {:x 15 :y 18} (Population. 15 15 15 15 15) initial-culture :tribe)
          p (possibility-of-splitting g t)]
     (is (< p 0.05))))
